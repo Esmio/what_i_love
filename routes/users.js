6722b1eeff4444e0564e6ecbbb53661bda8814b1
晓明 <18833897@qq.com>
2018-02-18 15:27:35 +0800

    add eslint

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-const users = []
+const router = express.Router();
+
 /* GET users listing. */
-router.route('/', function(req, res, next) {
-})
-  .get((req, res, next)=>{
-    res.json(users);
-  })
-  .post((req, res, next)=>{
-    const user = req.body;
-    users.push(user)
-    res.json(user);
-  })
-;
+router.get('/', (req, res) => {
+  res.send('response');
+});
 
 module.exports = router;
